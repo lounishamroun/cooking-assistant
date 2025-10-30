8fcf6cfc92d8a7d984f5105ca0708c4ebfeb23a7
Lounis Hamroun@gmail.com <lounis.hamroun@gmail.com>
2025-10-30 15:52:13 +0100

    feat: Prevent the downloading script to download the data if they already exists

--- cooking_assistant/utils/__init__.py
+++ cooking_assistant/utils/__init__.py
@@ -1,8 +1,9 @@
 """Module utilitaires pour le package cooking_assistant."""
 
-from .results import save_rankings_by_type, save_final_csvs
+from .results import save_combined_results_by_type, save_top_results, save_all_type_results
 
-__all__ = [
-    'save_rankings_by_type',
-    'save_final_csvs',
+_all_ = [
+    'save_combined_results_by_type',
+    'save_top_results', 
+    'save_all_type_results',
 ]
